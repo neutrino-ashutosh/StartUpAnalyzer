4fc1036c2519f3c585620b5a05763ad20b363e30
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2025-01-24 17:45:38 +0530

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from "@vercel/analytics/react"
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -8,7 +9,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E8XFLG4GKQ"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-E8XFLG4GKQ');
+        </script>
+      </Head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
